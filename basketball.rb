d72e4f81623261e26b9a78472b5a21be1e5d5c9d
Željko Filipin <zeljko.filipin@gmail.com>
2012-08-09 12:31:20 +0200

    refactoring: move code to method

--- basketball.rb
+++ basketball.rb
@@ -6,11 +6,13 @@ def go_to_game
   browser.goto "http://www.google.com/doodles/basketball-2012"
   browser
 end
-def jump_and_shoot(browser, wait_before_shooting, time_between_jump_and_shoot)
-  sleep wait_before_shooting
-  browser.send_keys :space
-  sleep time_between_jump_and_shoot
-  browser.send_keys :space
+def jump_and_shoot(browser, number_of_balls, wait_before_shooting, time_between_jump_and_shoot)
+  number_of_balls.times do
+    sleep wait_before_shooting
+    browser.send_keys :space
+    sleep time_between_jump_and_shoot
+    browser.send_keys :space
+  end
 end
 def start_the_game(browser)
   browser.div(id: "hplogo").frame.div.click
@@ -18,19 +20,7 @@ end
 
 browser = go_to_game
 start_the_game(browser)
-
-5.times do
-  jump_and_shoot(browser, 1, 0.2)
-end
-
-4.times do
-  jump_and_shoot(browser, 1, 0.4)
-end
-
-4.times do
-  jump_and_shoot(browser, 1, 0.6)
-end
-
-2.times do
-  jump_and_shoot(browser, 1, 0.8)
-end
+jump_and_shoot(browser, 5, 1, 0.2)
+jump_and_shoot(browser, 4, 1, 0.4)
+jump_and_shoot(browser, 4, 1, 0.6)
+jump_and_shoot(browser, 2, 1, 0.8)
