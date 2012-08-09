88e13d0e05e12fbb0e2f900d6ce612e5b8b81149
Željko Filipin <zeljko.filipin@gmail.com>
2012-08-09 12:27:39 +0200

    refactoring: move code to methods

--- basketball.rb
+++ basketball.rb
@@ -1,18 +1,23 @@
+def go_to_game
+  require "bundler/setup"
+  require "watir-webdriver"
+
+  browser = Watir::Browser.new :chrome
+  browser.goto "http://www.google.com/doodles/basketball-2012"
+  browser
+end
 def jump_and_shoot(browser, wait_before_shooting, time_between_jump_and_shoot)
   sleep wait_before_shooting
   browser.send_keys :space
   sleep time_between_jump_and_shoot
   browser.send_keys :space
 end
+def start_the_game(browser)
+  browser.div(id: "hplogo").frame.div.click
+end
 
-require "bundler/setup"
-require "watir-webdriver"
-
-# open browser and go to the page
-browser = Watir::Browser.new :chrome
-browser.goto "http://www.google.com/doodles/basketball-2012"
-
-browser.div(id: "hplogo").frame.div.click
+browser = go_to_game
+start_the_game(browser)
 
 5.times do
   jump_and_shoot(browser, 1, 0.2)
