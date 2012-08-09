45a03ee3f39f95a493de472a9a161ac4053ef242
Željko Filipin <zeljko.filipin@gmail.com>
2012-08-09 12:59:36 +0200

    the simplest code to finish the game
    score: 43,5s

--- canoe.rb
+++ canoe.rb
@@ -1,4 +1,15 @@
 require_relative "olympics"
 
+def paddle(browser, number)
+  number.times do
+    puts "Left!"
+    browser.send_keys :left
+    puts "Right!"
+    browser.send_keys :right
+  end
+end
+
 browser = go_to_game("slalom-canoe")
+sleep 1
 start_the_game(browser)
+paddle(browser, 100)
