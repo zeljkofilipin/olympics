78e2f0009cb602dbca23d1cadb54daa55961481a
Željko Filipin <zeljko.filipin@gmail.com>
2012-08-11 12:14:08 +0200

    move the goalie with mouse

--- lib/soccer.rb
+++ lib/soccer.rb
@@ -1,10 +1,26 @@
 require_relative "olympics"
 
+def center_mouse(browser)
+  browser.driver.action.move_to(browser.driver.find_element(:id, 'hplogo')).perform
+end
+def left(browser)
+  puts "Left!"
+  browser.driver.action.move_by(-100, 0).perform
+end
+def right(browser)
+  puts "Rigth!"
+  browser.driver.action.move_by(100, 0).perform
+end
+def tutorial(browser)
+  sleep 1
+  center_mouse(browser)
+  left(browser)
+  sleep 1
+  right(browser)
+  sleep 1
+  space(browser)
+end
+
 browser = go_to_game("soccer")
 start_the_game(browser)
-sleep 1
-left(browser)
-sleep 1
-right(browser)
-sleep 1
-space(browser)
+tutorial(browser)
