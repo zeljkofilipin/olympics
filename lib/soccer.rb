dbb032f57756b6d51d24e03d164afa106532ebb5
Željko Filipin <zeljko.filipin@gmail.com>
2012-08-11 13:07:41 +0200

    move randomly left or right, or jump

--- lib/soccer.rb
+++ lib/soccer.rb
@@ -20,7 +20,20 @@ def tutorial(browser)
   sleep 1
   space(browser)
 end
+def move(browser)
+  directions = {
+    0 => "left",
+    1 => "right",
+    2 => "space",
+  }
+  direction = Random.rand(0..2)
+  send directions[direction], browser
+  sleep 1
+end
 
 browser = go_to_game("soccer")
 start_the_game(browser)
 tutorial(browser)
+100.times do
+  move(browser)
+end
